7fd5dc36832b430b2520e59182847048e8bd3d1b
Jivika Kaushik <kaushikjivika@gmail.com>
2026-04-24 18:28:12 +0530

    Document ALTER TABLE usage in SQL

---
+++ Table_Related_Queries/02_ALTER TABLE.sql
@@ -0,0 +1,12 @@
+-- What is ALTER TABLE?
+-- ALTER TABLE is used to modify an existing table structure without deleting it.
+
+-- You can:
+
+-- 1. Add columns
+-- 2. Modify columns
+-- 3. Delete columns
+-- 4. Rename table / columns
+-- 5. Add or remove constraints
+
+-- END-
